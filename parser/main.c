805db4257fc5cbecfee39f558a47aba54219f9f6
Liams-theCreator <mmeellaallii1@gmail.com>
2025-06-20 17:32:47 +0100

    Simple test main for lexer

--- parser/main.c
+++ parser/main.c
@@ -6,18 +6,26 @@
 /*   By: imellali <imellali@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/20 16:17:44 by imellali          #+#    #+#             */
-/*   Updated: 2025/06/20 16:22:58 by imellali         ###   ########.fr       */
+/*   Updated: 2025/06/20 17:26:22 by imellali         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.h"
 
-int main(int argc, const char **argv)
+int	main(void)
 {
-    (void)argc;
-    (void)argv;
-    char *test = "hey";
-    int len = ft_strlen(test);
-    printf("%s > len = %d\n", test, len);
-    return 0;
+	char	*input;
+	size_t	n;
+	ssize_t	nread;
+
+	n = 444;
+	input = malloc(sizeof(char) * n);
+	if (!input)
+		return (-1);
+	nread = getline(&input, &n, stdin);
+	if (nread == -1)
+		return (-1);
+	printf("Command : %s\n", input);
+	free(input);
+	return (0);
 }
