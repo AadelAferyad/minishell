718955c30a13e97f87a43edc07b3780925c113ce
Liams-theCreator <mmeellaallii1@gmail.com>
2025-06-20 17:32:47 +0100

    Simple test main for lexer

--- parser/parser.h
+++ parser/parser.h
@@ -6,16 +6,19 @@
 /*   By: imellali <imellali@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/20 16:19:07 by imellali          #+#    #+#             */
-/*   Updated: 2025/06/20 16:31:58 by imellali         ###   ########.fr       */
+/*   Updated: 2025/06/20 17:27:28 by imellali         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSER_H
 # define PARSER_H
 
-/* Functions's libraries */
+/* Own Libraries Included */
 
 # include "../libft/libft.h"
+
+/* Functions's libraries */
+
 # include <fcntl.h>
 # include <readline/history.h>
 # include <readline/readline.h>
