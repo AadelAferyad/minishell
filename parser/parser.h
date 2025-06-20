caf07c49d9d30a56d3b2b1a361ea2af50e267bab
Liams-theCreator <mmeellaallii1@gmail.com>
2025-06-20 16:33:36 +0100

    Creating the Makefile and a simple header file

--- parser/parser.h
+++ parser/parser.h
@@ -1,23 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   parser.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: imellali <imellali@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/06/20 16:17:44 by imellali          #+#    #+#             */
-/*   Updated: 2025/06/20 16:22:58 by imellali         ###   ########.fr       */
+/*   Created: 2025/06/20 16:19:07 by imellali          #+#    #+#             */
+/*   Updated: 2025/06/20 16:31:58 by imellali         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "parser.h"
+#ifndef PARSER_H
+# define PARSER_H
 
-int main(int argc, const char **argv)
-{
-    (void)argc;
-    (void)argv;
-    char *test = "hey";
-    int len = ft_strlen(test);
-    printf("%s > len = %d\n", test, len);
-    return 0;
-}
+/* Functions's libraries */
+
+# include "../libft/libft.h"
+# include <fcntl.h>
+# include <readline/history.h>
+# include <readline/readline.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/stat.h>
+# include <sys/wait.h>
+# include <unistd.h>
+
+#endif
