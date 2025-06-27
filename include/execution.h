b943c6c5639a6e6590564007f0ddbde70cf5573c
AadelAferyad <animevineu@gmail.com>
2025-06-27 10:52:38 +0100

    builting funcs

--- include/execution.h
+++ include/execution.h
@@ -6,7 +6,7 @@
 /*   By: aaferyad <aaferyad@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/23 12:14:38 by aaferyad          #+#    #+#             */
-/*   Updated: 2025/06/24 11:53:57 by aaferyad         ###   ########.fr       */
+/*   Updated: 2025/06/26 11:57:22 by aaferyad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,20 @@
 
 typedef struct	environment
 {
-
+	char	*key;
+	char	*value;
+	struct environment	*next;
 } t_env;
 
 typedef struct golobal_struct
 {
 	t_collector	*collector;
 	/*t_parser	*parser;*/
-	/*t_env		*env;*/
+	t_env		*env;
 }	t_global;
 
 extern t_global	g_structs;
+
+void	create_env(char **env);
 
 #endif
