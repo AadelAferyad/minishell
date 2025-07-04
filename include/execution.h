fda2ef9bd1f6a88b8e619152c3f4810e43150387
Liams-theCreator <mmeellaallii1@gmail.com>
2025-07-04 17:52:14 +0100

    Add environement struct

--- include/execution.h
+++ include/execution.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   execution.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aaferyad <aaferyad@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: imellali <imellali@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/23 12:14:38 by aaferyad          #+#    #+#             */
-/*   Updated: 2025/06/24 11:53:57 by aaferyad         ###   ########.fr       */
+/*   Updated: 2025/07/04 16:10:32 by imellali         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,18 @@
 
 typedef struct	environment
 {
-
+	char	*key;
+	char	*value;
+	struct environment	*next;
 } t_env;
 
 typedef struct golobal_struct
 {
 	t_collector	*collector;
-	/*t_parser	*parser;*/
-	/*t_env		*env;*/
+	t_env	*env;
 }	t_global;
+
+void	create_env(char **env);
 
 extern t_global	g_structs;
 
